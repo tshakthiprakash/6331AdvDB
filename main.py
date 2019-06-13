c6acaa12e974c3fe0d55891d92d8f4c8dd961a65
Shakthi Prakash Thiruvathilingam <tshakthiprakash@gmail.com>
2019-06-12 22:32:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 port = int(os.getenv('PORT', 8000))
 @app.route('/')
 def home():
-   return render_template('home.html')
+   return "Hello World"
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=port,debug = False)
+   app.run()
