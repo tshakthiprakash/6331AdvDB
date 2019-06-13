5f59d8de37547c9990374f76569f8a5e05526c16
Shakthi Prakash Thiruvathilingam <tshakthiprakash@gmail.com>
2019-06-12 22:59:52 -0500

    removed port

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 application = app = Flask(__name__)
 import os
 
-port = int(os.getenv('PORT', 8000))
 @app.route('/')
 def home():
    return "Hello World"
