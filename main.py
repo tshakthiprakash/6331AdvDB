467fe1209a71fb0f67386cc342bd9500bfdc624f
tshakthiprakash <tshakthiprakash@gmail.com>
2019-06-14 14:46:37 -0500

    redis first push

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import pandas as pd
 application = app = Flask(__name__)
 import os
 import time
+import redis
+import pickle as pickle
 
 con = sql.connect("database.db")
+rd = redis.StrictRedis(host='shakthi8112.redis.cache.windows.net', port=6380, db=0,password='ncP8NFImWyXmxKjo1MOVoAmJg7KRFX7511MbiSFHR9k=',ssl=True)
 @app.route('/')
 def home():
    return render_template('home.html')
@@ -27,14 +30,27 @@ def addrec():
 @app.route('/list')
 def list():
 	start_time = time.time()
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	cur.execute("select * from Earthquake")
-	rows = cur.fetchall()
+	query = "select * from Earthquake"
+	if rd.get(query):
+		print("cached if")
+		rows = pickle.loads(rd.get("result"))
+	else :
+		print("else")
+		con = sql.connect("database.db")
+		cur = con.cursor()
+		cur.execute(query)
+		rows = cur.fetchall()
+		rd.set(query,pickle.dumps(rows))
 	end_time = time.time()
 	act_time = end_time - start_time
 	print(act_time)
 	return render_template("results.html",row = rows,act_time=act_time)
+
+@app.route('/check')
+def check():
+	if rd.exists('PORTAL_CONSOLE'):
+		print('success')
+	return render_template("home.html")
 
 if __name__ == '__main__':
    app.run()
