f595daaa0b88d8866e02f92c864df1034637dae2
tshakthiprakash <tshakthiprakash@gmail.com>
2019-06-13 22:05:00 -0500

    sqlite push

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 from flask import *
+import sqlite3 as sql
+import pandas as pd
 application = app = Flask(__name__)
 import os
+import time
 
+con = sql.connect("database.db")
 @app.route('/')
 def home():
    return render_template('home.html')
+  
+@app.route('/enternew')
+def upload_csv():
+   return render_template('upload.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+	   con = sql.connect("database.db")
+	   csv = request.files['myfile']
+	   file = pd.read_csv(csv)
+	   file.to_sql('Earthquake', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)
+	   con.close()
+	   return render_template('home.html')
+
+@app.route('/list')
+def list():
+	start_time = time.time()
+	con = sql.connect("database.db")
+	cur = con.cursor()
+	cur.execute("select * from Earthquake")
+	rows = cur.fetchall()
+	end_time = time.time()
+	act_time = end_time - start_time
+	print(act_time)
+	return render_template("results.html",row = rows)
 
 if __name__ == '__main__':
    app.run()
