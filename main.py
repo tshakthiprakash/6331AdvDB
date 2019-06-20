1288946e96ef3be0ba9f9e2e1496a98d96d21ad5
tshakthiprakash <tshakthiprakash@gmail.com>
2019-06-20 16:59:59 -0500

    push

--- main.py
+++ main.py
@@ -48,9 +48,7 @@ def addrec():
 	   return render_template('home.html')
 
 	
-@app.route('/q1search')
-def q1search():
-	return render_template('q1search.html')
+
 
 @app.route('/q1',methods = ['POST', 'GET'])
 def q1():
@@ -128,22 +126,36 @@ def clustering():
 	plt.ylabel('No of Eq', fontsize=15)
 	plot = convert_fig_to_html(fig)
 	return render_template("clus_o.html",data=plot.decode('utf8'))
+
+@app.route('/q1search')
+def q1search():
+	return render_template('q1search.html')
 	
-@app.route('/clustering_pie')
+@app.route('/clustering_pie',methods = ['POST', 'GET'])
 def clustering_pie():	
+	n = int(request.form["num"])
 	main_result = []
-	for i in range(0,10,2):
-		result = []
-		query = "SELECT count(*) FROM Earthquake where mag between "+str(i)+" and "+str(i+2)
-		con = sql.connect("database.db") 
-		cur = con.cursor()
-		cur.execute(query)
-		rows = cur.fetchone()
-		t = str(i)+ "-" + str(i+2)
-		result.append(t)
-		result.append(rows[0])
-		main_result.append(result)
-		#print(main_result)
+	result = []
+	query = "SELECT * FROM voting "
+	con = sql.connect("database.db") 
+	cur = con.cursor()
+	cur.execute(query)
+	rows = cur.fetchall()
+	indresult = []
+	for i in range(0,len(rows)):
+		if (rows[i][5]/rows[i][2])*100 >= 40 and (rows[i][5]/rows[i][2])*100 <=80 :
+			indresult.append(rows[i])
+	for j in range(40,80):
+		count = 0
+		t = str(j)+str(j+n)
+		for i in range(0,len(indresult)):
+			
+			if (indresult[i][5]/rows[i][2])*100 <= j and (indresult[i][5]/rows[i][2])*100 >= j+n :
+				count = count + 1
+	result.append(t)
+	result.append(count)
+	main_result.append(result)
+	print(main_result)
 	y=pd.DataFrame(main_result)
 	X= y.dropna()
 	fig=plt.figure()
@@ -152,34 +164,35 @@ def clustering_pie():
 	plot = convert_fig_to_html(fig)
 	return render_template("clus_o.html",data=plot.decode('utf8'))
 	
-@app.route('/clustering_scatter')
+@app.route('/q2search')
+def q2search():
+	return render_template('q2search.html')
+	
+@app.route('/clustering_scatter',methods = ['POST', 'GET'])
 def clustering_scatter():
-
-	query = "SELECT latitude,longitude FROM Earthquake "
-	con = sql.connect("database.db") 
-	cur = con.cursor()
-	cur.execute(query)
-	rows = cur.fetchall()
+	rows = []
+	mainres = []
+	n1  = int(request.form["n1"])
+	n2  = int(request.form["n2"])
+	for j in range(n1,n2):
+		resx = (j*j)+1
+		rows.append(resx)
+		#mainres.append(rows)
 	y=pd.DataFrame(rows)
+	print(y)
 	X= y.dropna()
-	print(X)
-	k=KMeans(n_clusters=5,random_state=0).fit(X)
+	#print(X)
+	'''k=KMeans(n_clusters=5,random_state=0).fit(X)
 	c=k.cluster_centers_
 	l=k.labels_
 	fig=plt.figure()
 	m=[]
 	plt.scatter(X[0],X[1],c=l)
-	plt.scatter(c[:,0],c[:,1],c='r',s=100,marker='x')
-	for i in  range(len(c)):
-		temp=[]
-		t = "c"+str(i)
-		m.append(t)
-		for j in range(0,len(c)):
-			if i!=j :
-				temp.append(distance.euclidean(c[i],c[j]))
-		m.append(temp)
+	plt.scatter(c[:,0],c[:,1],c='r',s=100,marker='x')'''
+	fig=plt.figure()
+	plt.plot(X[0],marker='o',markerfacecolor='red',markersize=6,color='blue',linewidth=1,linestyle='dashed')
 	plot = convert_fig_to_html(fig)
-	return render_template("clus_o.html",data=plot.decode('utf8'),distance = m)
+	return render_template("clus_o.html",data=plot.decode('utf8'))
 
 @app.route('/plot_line',methods=['GET','POST'])
 def plot_line():
