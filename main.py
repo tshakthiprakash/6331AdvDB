9e170ad003428c515b74972a3671d510ccbe1698
Shakthi Prakash Thiruvathilingam <tshakthiprakash@gmail.com>
2021-09-17 16:41:52 -0500

    update 1

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def home():
 	query = "SELECT * FROM voting where TotalPop between 1000 and 5000 "
 	con = sql.connect("database.db") 
 	cur = con.cursor()
-	print('hiii')
+	print('hiiiii')
 	cur.execute(query)
 	result = cur.fetchall()
 	return render_template('home.html',rows = rows,data = result)
